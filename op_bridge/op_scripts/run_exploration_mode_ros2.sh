ae9a9461994b892e6c6104ffd47f85a0a6dc57af
BarryWu_0222 <barrywu0222@gmail.com>
2023-12-22 20:50:02 +0800

    Town10HD map

--- op_bridge/op_scripts/run_exploration_mode_ros2.sh
+++ op_bridge/op_scripts/run_exploration_mode_ros2.sh
@@ -5,7 +5,7 @@
 #############################
 
 # export SIMULATOR_LOCAL_HOST="localhost"
-export SIMULATOR_LOCAL_HOST="192.168.11.5"
+export SIMULATOR_LOCAL_HOST="192.168.91.7"
 export SIMULATOR_PORT="2000"
 export TEAM_AGENT=${LEADERBOARD_ROOT}/op_bridge/op_ros2_agent.py
 export PYTHONPATH="${CARLA_ROOT}/PythonAPI/carla/":"${SCENARIO_RUNNER_ROOT}":"${LEADERBOARD_ROOT}":${PYTHONPATH}
@@ -20,13 +20,13 @@ export AGENT_ROLE_NAME="hero"
 export OP_BRIDGE_MODE="free" 
 
 # CARLA town name or custom OpenDRIVE absolute path, when BRIDGE_MODE is free 
-export FREE_MAP_NAME="Town01" 
+export FREE_MAP_NAME="Town10HD" 
 
 # Spawn point for the autonomous agent, when BRIDGE_MODE is free 
 # "x,y,z,roll,pitch,yaw"
 # Empty string means random starting position
 #export FREE_AGENT_POSE="175.4,195.14,0,0,0,180" 
-export FREE_AGENT_POSE=""
+export FREE_AGENT_POSE="-50,-45,0,0,0,90"
 
 gnome-terminal -- bash -c roscore
 python3 ${LEADERBOARD_ROOT}/op_bridge/op_bridge_ros2.py
